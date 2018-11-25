68098d11fda0b9bdaeca9a20679cb91cbd1f1bca
DanDevG <dan.dev.mail@gmail.com>
2018-11-25 10:51:40 +0000

    Added global openCube function

---
+++ open-cube.js
@@ -0,0 +1,16 @@
+/*!
+
+open-cube v0.0.1
+
+Copyright (c) 2018 Dan Dev
+
+Released under the MIT license
+https://github.com/DanDevG/open-cube/master/LICENSE.md
+
+*/
+
+(function() {
+	this.openCube = function(container, width, faces) {
+		
+	};
+}());
